06b7a93f581ae41b3a27589a9e3e4754dcea52e6
Roger O'Donnell <71945647+Roger-ODonnell@users.noreply.github.com>
2025-02-21 10:26:24 +0000

    Added Prompts to JavaScript maths Script

---
+++ Foundations/JavaScript/script.js
@@ -0,0 +1,28 @@
+// console.log(23 + 97);
+
+// console.log((4 + 6 + 9) / 77);
+
+// let a = 10
+
+// console.log(a)
+
+// a = 12;
+
+// console.log(a);
+
+// let b = 7 * a
+
+// console.log(b);
+
+// const max = 57;
+
+// const actual  = max - 13;
+
+// const percentage = actual / max;
+
+// console.log(percentage);
+
+// let a = +prompt("First number?", 1);
+// let b = +prompt("Second number?", 2);// Use + infront of prompt or + infront of a and b in alert
+
+// alert(+a + +b); // 12
